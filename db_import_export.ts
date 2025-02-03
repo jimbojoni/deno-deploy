b40e2408a29d2e4bdab4aeb42e2e7756a1650233
jimbojoni <jimbojoni@gmail.com>
2025-02-03 16:11:56 +0700

    Update db_import_export.ts

--- db_import_export.ts
+++ db_import_export.ts
@@ -1,4 +1,4 @@
-kimport { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
 const SUPABASE_KEY = Deno.env.get("SUPABASE_ANON_KEY")!;
