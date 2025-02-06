57aa840c1af11b344ccdb38121ffd88cb6553d26
jimbojoni <jimboboni@gmail.com>
2025-02-06 04:14:24 +0000

    tryna fix eta error

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ const router = new Router();
 
 // Route for the main page (index.html)
 router.get("/", async (context) => {
-  const html = await eta.renderFile("index.eta", {});
+  const html = await eta.renderFile("index.html", {});
   context.response.body = html;
   context.response.type = "text/html";
 });
