9ef9d033fe2c82098a867bad07a1760575fd9470
jimbojoni <jimboboni@gmail.com>
2025-02-08 12:18:31 +0000

    moduling

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { importSupabaseData, clearDenoKv, getDatabaseSize, } from "./db_import_e
 import { backupDenoKvToDrive, listAllFiles, deleteFile, deleteAllFilesAndFolders, } from "./google_utils.ts";
 import { authMiddleware, authLogin, } from "./auth.ts";
 import { displayArticle, postArticle, displayAllArticles } from "./article.ts";
-import { serveStatic } from "hono/bun"; 
+import { serveStatic } from "hono/middleware.ts";
 
 // Trigger new Deployment
 
