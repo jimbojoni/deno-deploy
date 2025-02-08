7d91f7ea4dccc299abbda3eab86a723af9353cd7
jimbojoni <jimboboni@gmail.com>
2025-02-08 13:50:27 +0000

    implement tui editor 1.1

--- html/js/create-article.js
+++ html/js/create-article.js
@@ -1,15 +1,13 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const editor = new toastui.Editor({
-      el: document.querySelector("#editor"),
-      height: "300px",
-      initialEditType: "markdown",
-      previewStyle: "vertical"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+	const editor = new toastui.Editor({
+		el: document.querySelector("#editor"),
+		height: "300px",
+		initialEditType: "markdown",
+		previewStyle: "vertical"
+	});
 
-    // Update hidden textarea before form submission
-    document.querySelector("form").addEventListener("submit", function () {
-      document.querySelector("#content-input").value = editor.getMarkdown();
-    });
-  });
-</script>
+	// Update hidden textarea before form submission
+	document.querySelector("form").addEventListener("submit", function () {
+		document.querySelector("#content-input").value = editor.getMarkdown();
+	});
+});
