a7c932071a339adbe3a5718d5cd8be499147568f
jimbojoni <jimboboni@gmail.com>
2025-02-10 05:14:49 +0000

    implement supabase login auth 1.1

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 //import { getCookie } from "https://deno.land/x/hono@v4.3.11/helper.ts";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
-const SUPABASE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 eta.configure({ views: "./html" });
 
