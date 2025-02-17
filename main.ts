ca79d32d1ff77409af206da40c71fe7d40648abe
jimbojoni <jimboboni@gmail.com>
2025-02-17 09:45:38 +0000

    logging env

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
+/*const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
 const SUPABASE_KEY = Deno.env.get("SUPABASE_ANON_KEY");
 const SECRET_KEY = Deno.env.get("SECRET_KEY") || "";
 const credentialsBase64 = Deno.env.get("GOOGLE_API_SIMOOL");
@@ -6,4 +6,12 @@ const credentialsBase64 = Deno.env.get("GOOGLE_API_SIMOOL");
 console.log("SUPABASE_URL:", SUPABASE_URL);
 console.log("SUPABASE_ANON_KEY:", SUPABASE_KEY);
 console.log("SECRET_KEY:", SECRET_KEY);
-console.log("GOOGLE_API_SIMOOL:", credentialsBase64);
+console.log("GOOGLE_API_SIMOOL:", credentialsBase64);*/
+
+const testEnvVar = Deno.env.get("SUPABASE_URL");
+
+if (!testEnvVar) {
+  console.log("SUPABASE_URL is not set.");
+} else {
+  console.log("SUPABASE_URL is:", testEnvVar);
+}
