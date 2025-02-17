70f1c224c3f292dc93f69843fd9d035352265169
jimbojoni <jimboboni@gmail.com>
2025-02-17 09:48:08 +0000

    logging env

--- main.ts
+++ main.ts
@@ -8,10 +8,32 @@ console.log("SUPABASE_ANON_KEY:", SUPABASE_KEY);
 console.log("SECRET_KEY:", SECRET_KEY);
 console.log("GOOGLE_API_SIMOOL:", credentialsBase64);*/
 
-const testEnvVar = Deno.env.get("SUPABASE_URL");
+import { Hono } from "https://deno.land/x/hono/mod.ts";
 
-if (!testEnvVar) {
-  console.log("SUPABASE_URL is not set.");
-} else {
-  console.log("SUPABASE_URL is:", testEnvVar);
-}
+const app = new Hono();
+
+app.get('/', (c) => {
+  const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
+  const SUPABASE_KEY = Deno.env.get("SUPABASE_ANON_KEY");
+  const SECRET_KEY = Deno.env.get("SECRET_KEY");
+  const credentialsBase64 = Deno.env.get("GOOGLE_API_SIMOOL");
+
+  return c.html(`
+    <html>
+      <head>
+        <title>Environment Variables</title>
+      </head>
+      <body>
+        <h1>Environment Variables</h1>
+        <ul>
+          <li><strong>SUPABASE_URL:</strong> ${SUPABASE_URL}</li>
+          <li><strong>SUPABASE_ANON_KEY:</strong> ${SUPABASE_KEY}</li>
+          <li><strong>SECRET_KEY:</strong> ${SECRET_KEY}</li>
+          <li><strong>GOOGLE_API_SIMOOL:</strong> ${credentialsBase64}</li>
+        </ul>
+      </body>
+    </html>
+  `);
+});
+
+export default app;
